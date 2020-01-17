45e7fb8171bf0f878f8c9832beb95dbea603caaa
amintoo <amin.daher@outlook.fr>
2020-01-17 17:47:41 +0100

    initialisation du repo

---
+++ projet-uml/src/app/person.ts
@@ -0,0 +1,26 @@
+class Person {
+
+  private _id: string;
+
+  protected name: string;
+
+  constructor() {
+    this._id = uuid.v4();
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getId(): string {
+    return this._id;
+  }
+
+  public static build(): Person {
+    return new Person();
+  }
+}
